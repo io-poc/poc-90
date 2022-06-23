9fc80960a19b1f1b78d4f4e7b293285743cf2798
Karn C <56947899+snps9225@users.noreply.github.com>
2022-06-23 15:06:58 -0400

    Update CustomAuthenticationProvider.java

--- src/main/java/org/hdivsamples/security/CustomAuthenticationProvider.java
+++ src/main/java/org/hdivsamples/security/CustomAuthenticationProvider.java
@@ -63,7 +63,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		}
 
 		User user = new User(listAccounts.get(0).getUsername(), listAccounts.get(0).getPassword(), authList);
-
+		System.out.println(listAccounts.get(0).getPassword());
 		return new UsernamePasswordAuthenticationToken(user, password, authList);
 	}
 

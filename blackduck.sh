76e4a3413b72bf68c94b4f885fc12366a109c568
Karn C <56947899+snps9225@users.noreply.github.com>
2022-06-23 15:24:27 -0400

    Update blackduck.sh

--- blackduck.sh
+++ blackduck.sh
@@ -6,4 +6,4 @@ wget -O detect.sh https://detect.synopsys.com/detect.sh
  
 chmod a+x detect.sh
  
-./detect.sh --blackduck.url=${{secrets.BLACKDUCK_URL}} --blackduck.api.token=${{secrets.BLACKDUCK_TOKEN}} --detect.project.name="insecure-bank-gha" --detect.project.version.name="1.0"  --detect.force.success=true
+./detect.sh --blackduck.url=$SYNOPSYS_IO_Blackduck_InstanceUrl --blackduck.api.token=$SYNOPSYS_IO_Blackduck_AuthToken --detect.project.name="insecure-bank-gha" --detect.project.version.name="1.0"  --detect.force.success=true

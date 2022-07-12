baca109056cea0c005fd1498374da83061bfcaed
Neil Pathare <neil.pathare@outlook.com>
2022-07-12 18:46:49 -0400

    Update CoreConfig.java

--- src/main/java/org/hdivsamples/config/CoreConfig.java
+++ src/main/java/org/hdivsamples/config/CoreConfig.java
@@ -6,5 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource({ "classpath:dataAccess-config.xml" })
 public class CoreConfig {
-
+  // Minor code commit - adding some code
+  String someString;
+  System.out.println("testing function add");
 }
